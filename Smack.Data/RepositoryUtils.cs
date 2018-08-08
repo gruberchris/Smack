74342f88cfd10e48e31d20606b30dfd36d935fd5
Chris Gruber <chris@chrisgruber.com>
2018-08-07 19:43:27 -0500

    small class to hold util logic i expect to reuse

---
+++ Smack.Data/RepositoryUtils.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smack.Data
+{
+    internal static class RepositoryUtils
+    {
+        public static ObjectId GetInternalId(string topicId)
+        {
+            return ObjectId.TryParse(topicId, out var internalId) ? internalId : ObjectId.Empty;
+        }
+    }
+}
