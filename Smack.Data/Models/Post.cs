774b60c146e5f5731a1d9f7a9ae560554a94ddd2
Chris Gruber <chris@chrisgruber.com>
2018-08-09 20:23:32 -0500

    Post model

---
+++ Smack.Data/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Smack.Data.Models
+{
+    public class Post
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("postId")]
+        public string PostId { get; set; }
+
+        [BsonElement("text")]
+        public string Text { get; set; }
+
+        [BsonElement("createdOn")]
+        public DateTime CreatedOn { get; set; }
+
+        [BsonElement("lastModifiedOn")]
+        public DateTime LastModifiedOn { get; set; }
+
+        [BsonElement("ownerUserId")]
+        public string OwnerUserId { get; set; }
+    }
+}
