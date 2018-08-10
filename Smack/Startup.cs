29f060b89198741c9569d16856a0b60d36b6e85f
Chris Gruber <chris@chrisgruber.com>
2018-08-09 22:41:00 -0500

    configure cors for the Smack SPA

--- Smack/Startup.cs
+++ Smack/Startup.cs
@@ -20,6 +20,15 @@ namespace Smack
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
