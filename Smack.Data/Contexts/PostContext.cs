921190b0ee5ac3be356f5b87fbee7fa25657b718
Chris Gruber <chris@chrisgruber.com>
2018-08-09 21:47:08 -0500

    database context for Post model

---
+++ Smack.Data/Contexts/PostContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Smack.Data.Models;
+
+namespace Smack.Data.Contexts
+{
+    public class PostContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public PostContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+
+            _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Post> Posts => _database.GetCollection<Post>("Post");
+    }
+}
